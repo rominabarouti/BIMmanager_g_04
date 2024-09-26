4ead1dbd58f68ebeea24d577f85c3c4715dde05e
rominabarouti <s237241@dtu.dk>
2024-09-26 16:38:30 +0200

    updated main - group 2 6 and 8 working

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 import ifcopenshell
-from external.Group1.Heightfloor2floorRule import checkRule
-#from external.Group9.rules.wallRule import calculate_wall_area
+from external.Group1.Heightfloor2floorRule import checkRule as floor2FloorHeight
+from external.Group2.rules.deskRule import checkRule as deskRule
+from external.Group6.rules.curtainWalls import checkRule as curtainWalls
+from external.Group8.rules.numberofDoor import checkRule as numberOfDoors
+from external.Group8.rules.doorType import checkRule as doorTypes
 
 model_path = "C:/Users/romin/Desktop/Advanced Bilding information modelling/CES_BLD_24_06_ARC.ifc"
 model = ifcopenshell.open(model_path)
 
 
 # Group 1 
-
 print("---------Group 1 Results-----------")
-checkRule(model) # Calculates and prints Floor2floor heights
+floor2FloorHeight(model)
+
+# Group 2 
+print("---------Group 2 Results-----------")
+desk_results = deskRule(model)
+print(f"Total Desks: {desk_results[0]}")
+print(desk_results[1])
+
+
+# Group 6 
+print("---------Group 6 Results-----------")
+curtainWalls(model)
+
+# Group 8 
+print("---------Group 8 Results-----------")
+print(numberOfDoors(model))
+print(doorTypes(model))
 
 
 """ roofs = model.by_type('Ifcroof')
