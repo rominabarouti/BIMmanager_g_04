5125744ddb398e01f343ab70ff09dc80b45c4429
rominabarouti <s237241@dtu.dk>
2024-09-26 16:41:17 +0200

    clean up

--- main.py
+++ main.py
@@ -30,33 +30,3 @@ print(numberOfDoors(model))
 print(doorTypes(model))
 
 
-""" roofs = model.by_type('Ifcroof')
-print("Number of roofs", len(roofs))
-
-elevations = model.by_type('IfcBuildingStorey')
-print("Number of elevations", len(elevations))
-
-# Group 6
-print("---------Group 6 Results-----------")
-windows = model.by_type('IfcWindow')
-print("Number of windows", len(windows))
-
-plates = model.by_type('IfcPlate')
-print("Number of plates", len(plates))
-
-curtain_walls = model.by_type('IfcCurtainWall')
-print("Number of curtain walls", len(curtain_walls))
-
-# Group 8
-print("---------Group 8 Results-----------")
-doors = model.by_type('IfcDoor')
-print("Number of doors",len(doors))
-
-door_types = model.by_type('IfcDoorType')
-print("Number of door types",len(door_types))
-
-# Group 9
-print("---------Group 9 Results-----------")
-settings = ifcopenshell.geom.settings()  
-total_wall_area = calculate_wall_area(model, settings)
-print(f"Total wall area: {total_wall_area:.2f} square meters") """
