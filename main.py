8a67bf79f630ef3bf80e2b7f0a76bd3a10a36dc5
rominabarouti <s237241@dtu.dk>
2024-09-27 16:03:33 +0200

    latest changes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from external.Group2.rules.deskRule import checkRule as deskRule
 from external.Group6.rules.curtainWalls import checkRule as curtainWalls
 from external.Group8.rules.numberofDoor import checkRule as numberOfDoors
 from external.Group8.rules.doorType import checkRule as doorTypes
+from external.Group9.rules.wallRule import calculate_wall_area 
 
 model_path = "C:/Users/romin/Desktop/Advanced Bilding information modelling/CES_BLD_24_06_ARC.ifc"
 model = ifcopenshell.open(model_path)
@@ -29,4 +30,6 @@ print("---------Group 8 Results-----------")
 print(numberOfDoors(model))
 print(doorTypes(model))
 
-
+# Group 9 
+total_wall_area = calculate_wall_area(model)
+print(f"Total wall area: {total_wall_area:.2f} square meters")
